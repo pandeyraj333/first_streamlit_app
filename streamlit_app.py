aad5abe9222286cf86a654548a44b04d02b4c4b3
pandeyraj333 <139428980+pandeyraj333@users.noreply.github.com>
2023-08-19 15:11:45 +0530

    Update streamlit_app.py
    Added first 5 tames

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
 streamlit.title('My First Ark Playthrough')
+streamlit.header('First five Tames')
+streamlit.text('Dodo')
+streamlit.text('Moschops')
+streamlit.text('Trike')
+streamlit.text('Tek Parasaur')
+streamlit.text('Pteranodon')
