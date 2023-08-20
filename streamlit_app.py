fbf42595d8e50326b3b32c52d82f3b53578b9810
pandeyraj333 <139428980+pandeyraj333@users.noreply.github.com>
2023-08-20 10:20:11 +0530

    Update streamlit_app.py
    Error Correction

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 import pandas
 
-streamlit.title('My Parent's New Healthy Diner')
+streamlit.title("My Parent's New Healthy Diner")
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗Kale, Spinach and Rocket Smoothie')
