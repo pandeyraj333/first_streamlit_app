68feb7e054187410840e3654b77c5e30032aa5e9
pandeyraj333 <139428980+pandeyraj333@users.noreply.github.com>
2023-08-20 10:19:38 +0530

    Update streamlit_app.py
    Changed topic to breakfast and added dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,13 @@
 import streamlit
+import pandas
 
-streamlit.title('My First Ark Playthrough')
-streamlit.header('First five Tames')
-streamlit.text('Dodo')
-streamlit.text('Moschops')
-streamlit.text('Trike')
-streamlit.text('Tek Parasaur')
-streamlit.text('Pteranodon')
+streamlit.title('My Parent's New Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗Kale, Spinach and Rocket Smoothie')
+streamlit.text('🐔Hard Boiled Free Range Egg')
+streamlit.text('🥑🍞Avocado Toast')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
